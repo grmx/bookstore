a60605dcbff929519845f07d125a8135ec00d6e3
Max Grechko <max@grechko.org>
2015-07-14 21:59:08 +0300

    Add an email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,24 @@ RSpec.describe User, type: :model do
   describe 'validation' do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+
+    describe 'email format' do
+      context 'is valid' do
+        it do
+          should allow_value('a+b@bar.name', 'A_F-OO@b.a.org', 'foo@bar.COM',
+            'fst.lst@bar.ua', 'example@from.aero').for(:email)
+        end
+      end
+
+      context 'is invalid' do
+        it { should_not allow_value('example.user@foo.').for(:email) }
+        it { should_not allow_value('foo@bar+baz.com').for(:email) }
+        it { should_not allow_value('foo@bar_baz.com').for(:email) }
+        it { should_not allow_value('user@foo,com').for(:email) }
+        it { should_not allow_value('user_at_foo.org').for(:email) }
+      end
+    end
+
   end
 end
