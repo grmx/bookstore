45e48e5dc916ff5993bdaf899373a9c687ffded1
Max Grechko <max@grechko.org>
2015-08-10 14:29:31 +0300

    Fix Devise permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,11 +21,11 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:username, :email, :password, :password_confirmation,
+      u.permit(:email, :password, :password_confirmation,
         :remember_me, :avatar, :avatar_cache, :remove_avatar)
     end
     devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:username, :email, :password, :password_confirmation,
+      u.permit(:email, :password, :password_confirmation,
         :current_password, :avatar, :avatar_cache, :remove_avatar)
     end
   end
