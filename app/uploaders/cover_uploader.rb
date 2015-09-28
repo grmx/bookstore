c977ef508c3b883157478a9013dae76a157fd7be
Max Grechko <max@grechko.org>
2015-09-29 00:24:46 +0300

    Fix uploaders

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -1,7 +1,16 @@
 # encoding: utf-8
 class CoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  include Cloudinary::CarrierWave
+
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+  else
+    storage :file
+
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+  end
 
   process resize_to_limit: [400, 400]
 
